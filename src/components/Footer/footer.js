a6e9d1c0bf797f6e65dd48218f8891cf35bc7537
Kyle Patrick Bader <kbader91@gmail.com>
2022-11-09 17:10:57 -0800

    all pages set up and ready for initial deployment

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,54 @@
 import './footer.css';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 function Footer () {
 
-    // return (
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
 
-    // )
+    return (
+        <section id="footer">
+        {/* <img /> */}
+        <h2>Fremont Rehab</h2> 
+        <Nav id="footer-links">
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/fremont-rehab" className="footer-nav-tab" onClick={scrollToTop}>Home</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/treatment" className="footer-nav-tab" onClick={scrollToTop}>Treatment</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/mission" className="footer-nav-tab" onClick={scrollToTop}>Mission</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/about" className="footer-nav-tab" onClick={scrollToTop}>About</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/insurance" className="footer-nav-tab" onClick={scrollToTop}>Insurance</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/contact" className="footer-nav-tab" onClick={scrollToTop}>Contact</Link>
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    </section>
+    )
 };
 
 export default Footer;
--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,11 +1,48 @@
 import './navigation.css';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 function Navigation () {
 
-    // return (
+    return (
+        <section id="nav-bar">
+        {/* <img /> */}
+        <header>Fremont Rehab</header> 
+        <Nav id="main-nav-links">
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/fremont-rehab" className="header-nav-tab">Home</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/treatment" className="header-nav-tab">Treatment</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/mission" className="header-nav-tab">Mission</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/about" className="header-nav-tab">About</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/insurance" className="header-nav-tab">Insurance</Link>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <Link to="/contact" className="header-nav-tab">Contact</Link>
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    </section>
 
-
-    // )
+    )
 };
 
 export default Navigation;
