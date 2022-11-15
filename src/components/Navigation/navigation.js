e2e61f5c6d44aad9333eaac5fbc5e21f0ad4725d
Kyle Patrick Bader <kbader91@gmail.com>
2022-11-15 12:17:36 -0800

    hamburger menu added for mobile media queries

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,8 +1,17 @@
 import './navigation.css';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Navigation () {
+
+    // offCanvas 
+const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+// const onClick = () => setShow(true);
 
     return (
         <section id="nav-bar">
@@ -40,6 +49,30 @@ function Navigation () {
                 </Nav.Link>
             </Nav.Item>
         </Nav>
+
+        <Button id="hamburger-btn" className="openbtn" onClick={handleShow}>
+                ☰
+            </Button>
+
+
+            <Offcanvas placement="end" show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton />
+                <Offcanvas.Body>
+                    <section id="nav-tabs">
+                        <ul className="tabpanel">
+
+                            <Link to="/ocean-valley-bh" className="header-nav-tab">Home</Link>
+                            <Link to="/treatment" className="header-nav-tab">Treatment</Link>
+                            <Link to="/mission" className="header-nav-tab">Mission</Link>
+                            <Link to="/about" className="header-nav-tab">About</Link>
+                            <Link to="/insurance" className="header-nav-tab">Insurance</Link>
+                            <Link to="/contact" className="header-nav-tab">Contact</Link>
+
+                        </ul>
+                    </section>
+
+                </Offcanvas.Body>
+            </Offcanvas >
     </section>
 
     )
